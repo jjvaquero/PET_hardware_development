e7475cc689625b91c22b704d350130fb102c029c
rchil <rchil@f295ea29-772a-674d-bfef-7bcd4ff8141a>
2012-12-12 15:12:55 +0000

    annado lo de urjc

---
+++ Arduino_CSN/saca_histos.m
@@ -0,0 +1,16 @@
+% a ver si tengo espertroossssss
+arxivo = fopen('datos_leidos2.raw');
+%me aseguro que estoy en el comienzo de un nuevo histograma
+valor = fread(arxivo,1,'uint8');
+datos = zeros(50,1024);
+for i=1:50
+    while valor ~= 13 
+        valor = fread(arxivo,1,'uint8');
+    end
+    datos(i,:) = fread(arxivo,1024,'uint16');
+end
+
+fclose(arxivo);
+plot(datos(25,:));
+suma = sum(datos);
+figure; plot(suma);
