4cec0586bdc929ec8d5a91fc918729bff5f4b506
rchil <rchil@f295ea29-772a-674d-bfef-7bcd4ff8141a>
2013-09-05 16:40:02 +0000

    Al parecer la sincronización funciona bien...pero al usar algun elemento para reogarnizar los datos...se cuelga probar a reorganizarlos en la parte de PS...aunque no tiene mucho sentido lo que está pasando...usar sino un FDCE como buffer para los datos

---
+++ Zynq/Xapp_v3/Xapp_v3.srcs/sources_1/edk/module_1/module_1_stub.vhd
@@ -0,0 +1,289 @@
+-------------------------------------------------------------------------------
+-- module_1_stub.vhd
+-------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+
+entity module_1_stub is
+  generic(
+    C_AdcChnls          : integer := 2;     -- Number of ADC in a package 
+    C_AdcWireInt        : integer := 2     -- 2 = 2-wire, 1 = 1-wire interface
+    );
+  port (
+    processing_system7_0_MIO : inout std_logic_vector(53 downto 0);
+    processing_system7_0_PS_SRSTB_pin : in std_logic;
+    processing_system7_0_PS_CLK_pin : in std_logic;
+    processing_system7_0_PS_PORB_pin : in std_logic;
+    processing_system7_0_DDR_Clk : inout std_logic;
+    processing_system7_0_DDR_Clk_n : inout std_logic;
+    processing_system7_0_DDR_CKE : inout std_logic;
+    processing_system7_0_DDR_CS_n : inout std_logic;
+    processing_system7_0_DDR_RAS_n : inout std_logic;
+    processing_system7_0_DDR_CAS_n : inout std_logic;
+    processing_system7_0_DDR_WEB_pin : out std_logic;
+    processing_system7_0_DDR_BankAddr : inout std_logic_vector(2 downto 0);
+    processing_system7_0_DDR_Addr : inout std_logic_vector(14 downto 0);
+    processing_system7_0_DDR_ODT : inout std_logic;
+    processing_system7_0_DDR_DRSTB : inout std_logic;
+    processing_system7_0_DDR_DQ : inout std_logic_vector(31 downto 0);
+    processing_system7_0_DDR_DM : inout std_logic_vector(3 downto 0);
+    processing_system7_0_DDR_DQS : inout std_logic_vector(3 downto 0);
+    processing_system7_0_DDR_DQS_n : inout std_logic_vector(3 downto 0);
+    processing_system7_0_DDR_VRN : inout std_logic;
+    processing_system7_0_DDR_VRP : inout std_logic;
+    
+    --This signals will only be used inside, no need to route them to pins on the FPGA
+    --LED_DutyCycle : out std_logic_vector(31 downto 0);
+    --processing_system7_0_FCLK_CLK0_pin : out std_logic;
+    --ADC_DataIn : in std_logic_vector(31 downto 0);
+    --processing_system7_0_FCLK_RESET0_N_pin : out std_logic;
+    --processing_system7_0_FCLK_CLK1_pin : out std_logic;
+    --processing_system7_0_FCLK_RESET1_N_pin : out std_logic
+       LEDS : out std_logic_vector(7 downto 0); 
+       --pongo ademas el pin para la salida del reloj
+       FCLK :  in std_logic_vector(1 downto 0);
+       --relojes auxiliares para que planaahead no se los cargue
+       FCLKP1: out std_logic;  --reloj usado para el conversor,sal por JA4
+       --FCLKP2: out std_logic;  --TODO borrar...sale por JB4
+       --FCLKP3: out std_logic;  --sale por JB2
+       --used to control the sync pattern
+       SData: out std_logic;
+       --pines para datos
+       ADC_Data1: in std_logic_vector(1 downto 0);
+       ADC_Data0: in std_logic_vector(1 downto 0);
+      -- ADC_Data2: in std_logic_vector(1 downto 0);
+       --ADC_Data3: in std_logic_vector(1 downto 0);
+       DCLK: in std_logic_vector(1 downto 0)
+       --BTNC : in std_logic 
+       --BTNU : in std_logic
+    
+    
+  );
+end module_1_stub;
+
+architecture STRUCTURE of module_1_stub is
+
+    signal sLED_DutyCycle : std_logic_vector(31 downto 0);
+    signal sADC_DataIn : std_logic_vector(31 downto 0):=(others => '0');
+    --signal sADC_Data: std_logic_vector(11 downto 0);
+    signal botones : std_logic_vector(1 downto 0);
+    signal datBuff  : std_logic_vector(11 downto 0);
+    signal sprocessing_system7_0_FCLK_CLK0_pin : std_logic;
+    signal PL_Rst, sClkExtIn: std_logic;
+    signal sADC_Data1,  sADC_Data0, sDCLK, sFCLK: std_logic;
+    
+    --Xapp
+    signal IntRst       : std_logic;
+    signal IntBitClkDone : std_logic;
+    signal IntBitClk    : std_logic;
+    signal IntDivClk    : std_logic;
+    signal delayRefClk  : std_logic;
+
+    
+
+    
+
+        
+  component module_1 is
+    port (
+      processing_system7_0_MIO : inout std_logic_vector(53 downto 0);
+      processing_system7_0_PS_SRSTB_pin : in std_logic;
+      processing_system7_0_PS_CLK_pin : in std_logic;
+      processing_system7_0_PS_PORB_pin : in std_logic;
+      processing_system7_0_DDR_Clk : inout std_logic;
+      processing_system7_0_DDR_Clk_n : inout std_logic;
+      processing_system7_0_DDR_CKE : inout std_logic;
+      processing_system7_0_DDR_CS_n : inout std_logic;
+      processing_system7_0_DDR_RAS_n : inout std_logic;
+      processing_system7_0_DDR_CAS_n : inout std_logic;
+      processing_system7_0_DDR_WEB_pin : out std_logic;
+      processing_system7_0_DDR_BankAddr : inout std_logic_vector(2 downto 0);
+      processing_system7_0_DDR_Addr : inout std_logic_vector(14 downto 0);
+      processing_system7_0_DDR_ODT : inout std_logic;
+      processing_system7_0_DDR_DRSTB : inout std_logic;
+      processing_system7_0_DDR_DQ : inout std_logic_vector(31 downto 0);
+      processing_system7_0_DDR_DM : inout std_logic_vector(3 downto 0);
+      processing_system7_0_DDR_DQS : inout std_logic_vector(3 downto 0);
+      processing_system7_0_DDR_DQS_n : inout std_logic_vector(3 downto 0);
+      processing_system7_0_DDR_VRN : inout std_logic;
+      processing_system7_0_DDR_VRP : inout std_logic; 
+      LED_DutyCycle : out std_logic_vector(31 downto 0);
+      processing_system7_0_FCLK_CLK0_pin : out std_logic;
+      ADC_DataIn : in std_logic_vector(31 downto 0);
+      processing_system7_0_FCLK_RESET0_N_pin : out std_logic;
+      processing_system7_0_FCLK_CLK1_pin : out std_logic;
+      processing_system7_0_FCLK_RESET1_N_pin : out std_logic
+      --axi_gpio_1_GPIO_IO_I_pin : in std_logic_vector( 1 downto 0)
+    );
+  end component;
+  
+
+ attribute BOX_TYPE : STRING;
+ attribute BOX_TYPE of module_1 : component is "user_black_box";
+ 
+ 
+ component PWM_Controller is
+    port (
+     Clk : in std_logic;
+     DutyCycle : in std_logic_vector(31 downto 0);
+ 	 LED : out std_logic --_vector(6 downto 0)
+     );
+ end component;
+ 
+ component AdcClock is
+ 	generic (
+ 	   C_AdcBits   : integer := 12;
+ 	   C_StatTaps  : integer := 16
+ 	);
+     port (
+         BitClk				: in std_logic;
+         BitClkRst			: in std_logic;
+         BitClkEna			: in std_logic;
+         BitClkReSync		: in std_logic;
+         BitClk_MonClkOut	: out std_logic;   -- CLK output
+         BitClk_MonClkIn		: in std_logic;    -- ISERDES.CLK input
+         BitClk_RefClkOut	: out std_logic;   -- CLKDIV & logic output
+         BitClk_RefClkIn		: in std_logic;    -- CLKDIV & logic input
+         BitClkAlignWarn 	: out std_logic;
+ 		BitClkInvrtd		: out std_logic;
+         BitClkDone 			: out std_logic 
+     );
+ end component;
+ 
+  component ADCReadFSM is
+     Port ( DivClk : in STD_LOGIC;
+            BitClk : in STD_LOGIC;
+            bit0 : in STD_LOGIC;
+            bit1 : in STD_LOGIC;
+            Rst : in STD_LOGIC;
+            State : out std_logic_vector( 2 downto 0);
+            DataOut : out STD_LOGIC_VECTOR (11 downto 0));
+ end component;
+ 
+ 
+
+
+begin
+
+  --LVDS signal mapping
+    ibuf0 : IBUFDS  port map (I=>ADC_Data0(0), IB=>ADC_Data0(1), O=>sADC_Data0); 
+    ibuf1 : IBUFDS  port map (I=>ADC_Data1(0), IB=>ADC_Data1(1), O=>sADC_Data1);   
+    ibuf2 : IBUFGDS port map (I=>DCLK(0), IB=>DCLK(1), O=>sDCLK);
+    ibuf3 : IBUFGDS port map (I=>FCLK(0), IB=>FCLK(1), O=>sFCLK);
+    --ioClk : BUFIO   port map (I=> sDCLK, O => IntBitClk);
+    --IntDivClk <= sFCLK;
+    --ibuf1 : IBUF port map (I => BTNU, O=> botones(0));
+  
+
+  module_1_i : module_1
+    port map (
+      processing_system7_0_MIO => processing_system7_0_MIO,
+      processing_system7_0_PS_SRSTB_pin => processing_system7_0_PS_SRSTB_pin,
+      processing_system7_0_PS_CLK_pin => processing_system7_0_PS_CLK_pin,
+      processing_system7_0_PS_PORB_pin => processing_system7_0_PS_PORB_pin,
+      processing_system7_0_DDR_Clk => processing_system7_0_DDR_Clk,
+      processing_system7_0_DDR_Clk_n => processing_system7_0_DDR_Clk_n,
+      processing_system7_0_DDR_CKE => processing_system7_0_DDR_CKE,
+      processing_system7_0_DDR_CS_n => processing_system7_0_DDR_CS_n,
+      processing_system7_0_DDR_RAS_n => processing_system7_0_DDR_RAS_n,
+      processing_system7_0_DDR_CAS_n => processing_system7_0_DDR_CAS_n,
+      processing_system7_0_DDR_WEB_pin => processing_system7_0_DDR_WEB_pin,
+      processing_system7_0_DDR_BankAddr => processing_system7_0_DDR_BankAddr,
+      processing_system7_0_DDR_Addr => processing_system7_0_DDR_Addr,
+      processing_system7_0_DDR_ODT => processing_system7_0_DDR_ODT,
+      processing_system7_0_DDR_DRSTB => processing_system7_0_DDR_DRSTB,
+      processing_system7_0_DDR_DQ => processing_system7_0_DDR_DQ,
+      processing_system7_0_DDR_DM => processing_system7_0_DDR_DM,
+      processing_system7_0_DDR_DQS => processing_system7_0_DDR_DQS,
+      processing_system7_0_DDR_DQS_n => processing_system7_0_DDR_DQS_n,
+      processing_system7_0_DDR_VRN => processing_system7_0_DDR_VRN,
+      processing_system7_0_DDR_VRP => processing_system7_0_DDR_VRP,
+      LED_DutyCycle => sLED_DutyCycle,
+      processing_system7_0_FCLK_CLK0_pin => sprocessing_system7_0_FCLK_CLK0_pin,
+      ADC_DataIn => sADC_DataIn,
+      --processing_system7_0_FCLK_RESET0_N_pin => processing_system7_0_FCLK_RESET0_N_pin,
+      processing_system7_0_FCLK_CLK1_pin => sClkExtIn,
+      processing_system7_0_FCLK_RESET1_N_pin => PL_Rst
+    );
+    
+    
+    PWM:PWM_Controller 
+    port map(
+        Clk =>sprocessing_system7_0_FCLK_CLK0_pin,
+        DutyCycle =>sLED_DutyCycle,
+    	LED =>LEDS(7)
+    );
+    
+    AdcClk : AdcClock 
+      port map (
+            BitClk				=> sDCLK,
+            BitClkRst			=> IntRst,
+            BitClkEna			=> '1',
+            BitClkReSync		=> '0',
+            BitClk_MonClkOut	=> IntBitClk,   -- CLK output
+            BitClk_MonClkIn		=> IntBitClk,    -- ISERDES.CLK input
+            BitClk_RefClkOut	=> IntDivClk,   -- CLKDIV & logic output
+            BitClk_RefClkIn		=> IntDivClk,    -- CLKDIV & logic input
+            BitClkAlignWarn 	=> open,
+    		BitClkInvrtd		=> open,
+            BitClkDone 			=> IntBitClkDone 
+        );
+        
+     ADCReader : ADCReadFSM 
+         Port map ( 
+            DivClk      => sFCLK,
+            BitClk      => IntBitClk,
+            bit0        => sADC_Data0,
+            bit1        => sADC_Data1,
+            Rst         => IntRst,
+            State       => LEDS(4 downto 2),
+            DataOut     => datBuff --sADC_DataIn(11 downto 0)
+        );   
+        
+        -- IDELAYCTRL is needed for calibration
+        BUFG_DlyCtrl : BUFG
+            port map (
+            O => delayRefClk, -- 1-bit output: Clock output
+            I => sprocessing_system7_0_FCLK_CLK0_pin -- 1-bit input: Clock input
+            );
+        
+        AdcClk_delayctrl : IDELAYCTRL
+            port map (
+             --RDY    => DELAY_LOCKED,
+             REFCLK => delayRefClk,
+             RST    => IntRst
+             );
+    
+ 
+    
+    ----------
+    --AppsSysClk <= sprocessing_system7_0_FCLK_CLK0_pin;
+    --AppsSysEna <='1';
+    IntRst<= not(PL_Rst);
+    
+    
+
+
+    --sADC_DataIn(31 downto 12) <= (others =>'0');
+    sADC_DataIn (11 downto 0) <= datBuff;
+   --sADC_DataIn(11)<= datBuff(11); sADC_DataIn(9)<= datBuff(10); sADC_DataIn(7)<= datBuff(9); 
+   -- sADC_DataIn(5)<= datBuff(8); sADC_DataIn(3)<= datBuff(7); sADC_DataIn(1)<= datBuff(6); 
+   -- sADC_DataIn(10)<= datBuff(5); sADC_DataIn(8)<= datBuff(4); sADC_DataIn(6)<= datBuff(3); 
+    --sADC_DataIn(4)<= datBuff(2); sADC_DataIn(2)<= datBuff(1); sADC_DataIn(0)<= datBuff(0); 
+    
+    FCLKP1 <= sCLkExtIn;
+    SData <='1';
+    LEDS (1)<='0';
+    LEDS(6)<='0'; LEDS(5) <= '0'; -- <= (others=>'0');
+    LEDS(0)<= IntBitClkDone;
+    
+
+end architecture STRUCTURE;
+
---
+++ Zynq/Xapp_v3/Xapp_v3.srcs/sources_1/new/ADCReadFSM.vhd
@@ -0,0 +1,122 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/05/2013 12:18:14 PM
+-- Design Name: 
+-- Module Name: ADCReadFSM - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ADCReadFSM is
+    Port ( DivClk : in STD_LOGIC;
+           BitClk : in STD_LOGIC;
+           bit0 : in STD_LOGIC;
+           bit1 : in STD_LOGIC;
+           Rst : in STD_LOGIC;
+           State : out std_logic_vector( 2 downto 0);
+           DataOut : out STD_LOGIC_VECTOR (11 downto 0));          
+end ADCReadFSM;
+
+architecture Behavioral of ADCReadFSM is
+
+--states used for the FSM
+ type states is (S0,S1,S2,S3,S4,S5);
+ signal currState, nextState : states; 
+ 
+ signal datBit0     : std_logic_vector(5 downto 0);
+ signal datBit1     : std_logic_vector(5 downto 0);
+ signal intRst      : std_logic;
+
+begin
+
+    process (BitClk, Rst) --, intRst)
+        begin        
+           --if (rising_edge(BitClk)) then  --es DDR, luego prueba a usar rising_edge y SDR       
+            if ( Rst = '1')  then
+               currState <= S0; 
+               --intRst<='0';
+              else
+                 currState <= nextState;
+              end if;
+           --end if;
+          --if (rising_edge(intRst)) then
+              -- currState<= S0;
+          --end if;          
+           
+  end process;
+  
+   process(currState)
+   
+   begin
+   
+        case currState is
+            when S0=>
+                datBit1(5) <= bit1; datBit0(5) <=bit0;
+                nextState <= S1; State<= "001";
+            when S1=>
+                datBit1(4) <= bit1; datBit0(4) <=bit0;
+                nextState <= S2;  State<= "010";            
+            when S2=>
+                datBit1(3) <= bit1; datBit0(3) <=bit0;
+                nextState <= S3; State<= "011";
+            when S3=>
+                datBit1(2) <= bit1; datBit0(2) <=bit0;
+                nextState <= S4;  State<="100";             
+            when S4=>
+                datBit1(1) <= bit1; datBit0(1) <=bit0;
+                nextState <= S5;  State<="101";              
+            when S5=>
+                datBit1(0) <= bit1; datBit0(0) <=bit0;
+                nextState <= S0;  State<="110";
+        end case;                
+        
+   end process;
+   
+   process(DivClk)  
+   begin 
+      if (DivClk='1') then
+         --DataOut(11) <=datBit1(5); DataOut(9) <= datBit1(4); DataOut(7)<=datBit1(3);
+         --DataOut(5)<=datBit1(2); DataOut(3) <=datBit1(1); DataOut(1) <= datBit1(0);
+         
+          --DataOut(10) <=datBit0(5); DataOut(8) <= datBit0(4); DataOut(6)<=datBit0(3);
+          --DataOut(4)<=datBit0(2); DataOut(2) <=datBit0(1); DataOut(0) <= datBit0(0);
+         
+         DataOut(11 downto 6) <= datBit1;
+         DataOut (5 downto 0) <= datBit0;
+        --asignacion decente de los datos
+        intRst <='1';  
+     else 
+        intRst<='0';     
+     end if;
+     
+        --datBit0<=(others=>'0');
+        --datBit1<=(others=>'0');      
+   end process;
+    
+
+
+end Behavioral;
